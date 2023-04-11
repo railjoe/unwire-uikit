dd64844138063231fed95058aabce4e24fe202de
Jovan Stojanov <railjoe@gmail.com>
2023-04-11 16:26:16 +0200

    Set date parser locale

--- unwire/data/service/iTunesSearchAPIServiceImpl.swift
+++ unwire/data/service/iTunesSearchAPIServiceImpl.swift
@@ -16,6 +16,7 @@ class iTunesSearchAPIServiceImpl: iTunesSearchAPIService {
         self.restClient = restClient
         
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
         self.decoder.dateDecodingStrategy = .formatted(dateFormatter)
     }
